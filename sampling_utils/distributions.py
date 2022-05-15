75ae195ca6766e880e85ef6ae5c686ea5db72c54
Sergey Samsonov <52744661+svsamsonov@users.noreply.github.com>
2022-05-15 13:09:15 +0300

    update

--- sampling_utils/distributions.py
+++ sampling_utils/distributions.py
@@ -335,7 +335,7 @@ class Funnel(Distribution):
         self.a = (kwargs.get("a", 1.0) * torch.ones(1)).to(self.device)
         self.b = (kwargs.get("b", 0.5))
         self.dim = kwargs.get("dim", 16)
-        self.distr1 = torch.distributions.Normal(torch.zeros(1), self.a)
+        self.distr1 = torch.distributions.Normal(torch.zeros(1).to(self.device), self.a)
         # self.distr2 = lambda z1: torch.distributions.MultivariateNormal(torch.zeros(self.dim-1), (2*self.b*z1).exp()*torch.eye(self.dim-1))
         # self.distr2 = lambda z1: -(z[...,1:]**2).sum(-1) * (-2*self.b*z1).exp() - np.log(self.dim) + 2*self.b*z1
         self.xlim = [-2, 10]
