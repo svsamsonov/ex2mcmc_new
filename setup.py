95ae0b6819f1c5480d3d538e5e5e06d562e563f6
evgeny lagutin <lagutin.em@phystech.edu>
2023-04-17 18:46:40 +0300

    refresh

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="",
     packages=find_packages(exclude=["tests*"]),
     install_requires = [
-            'torch-mimicry @ git+https://github.com/kwotsin/mimicry.git@a7fda06c4aff1e6af8dc4c4a35ed6636e434c766',
+            'torch-mimicry @ git+https://github.com/kwotsin/mimicry.git',
             'tqdm',
             'pyro-ppl',
             'jaxlib',
