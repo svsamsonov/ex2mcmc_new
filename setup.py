fcb13621cd19df2d4631f7da9266f61be46d0646
evgeny lagutin <lagutin.em@phystech.edu>
2023-04-17 17:24:25 +0300

    refresh

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-from os.path import abspath, dirname, join
-
-import toml
+from pathlib import Path
 from setuptools import find_packages, setup
 
-with open("pyproject.toml", "r") as f:
-    requirements = toml.loads(f.read())
+requirements = Path('requirements.txt').open('r').readlines()
 
-prod = requirements["install_requires"]
-dev = requirements["extras_require"]["dev"]
-
-setup(
-    install_requires=[x + prod[x] if prod[x] != "*" else x for x in prod],
-    extras_require={"dev": [x + dev[x] if dev[x] != "*" else x for x in dev]},
-)
+# setup(
+#     name="foo",
+#     version="1.0",
+#     packages=find_packages(),
+#     install_requires=requirements,
+#     # extras_require={"dev": [x + dev[x] if dev[x] != "*" else x for x in dev]},
+# )
+setup()
